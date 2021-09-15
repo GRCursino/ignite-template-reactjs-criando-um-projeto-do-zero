2449d790347cfd3cb777106ec4a136056ef8fa5a
Guilherme Cursino <guiiix93@gmail.com>
2021-09-15 14:44:15 -0300

    :tada: challenge complete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,22 @@
+import { useState } from 'react';
+
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
 import { getPrismicClient } from '../services/prismic';
+import Prismic from '@prismicio/client';
+
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
 interface Post {
-  uid?: string;
+  uid: string;
   first_publication_date: string | null;
   data: {
     title: string;
@@ -24,13 +34,131 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps) {
+  const formattedPost = postsPagination.results.map(post => {
+    return {
+      ...post,
+      first_publication_date: format(
+        new Date(post.first_publication_date),
+        'PP',
+        {
+          locale: ptBR,
+        }
+      ),
+    };
+  });
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+  const [posts, setPosts] = useState<Post[]>(formattedPost);
+  const [nextPage, setNextPage] = useState(postsPagination.next_page);
+  const [currentPage, setCurrentPage] = useState(1);
 
-//   // TODO
-// };
+  async function handleNextPage(): Promise<void> {
+    if (currentPage !== 1 && nextPage === null) {
+      return;
+    }
+
+    const postsResults = await fetch(`${nextPage}`).then(response =>
+      response.json()
+    );
+
+    setNextPage(postsResults.next_page);
+    setCurrentPage(postsResults.page);
+
+    const newPosts = postsResults.results.map(post => {
+      return {
+        uid: post.uid,
+        first_publication_date: format(
+          new Date(post.first_publication_date),
+          'PP',
+          {
+            locale: ptBR,
+          }
+        ),
+        data: {
+          title: post.data.title,
+          subtitle: post.data.subtitle,
+          author: post.data.author,
+        },
+      };
+    });
+
+    setPosts([ ...posts, ...newPosts]);
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Home | spacetraveling</title>
+      </Head>
+
+      <main className={commonStyles.container}>
+        <div className={styles.posts}>
+          {posts.map(post => {
+            return (
+              <Link href={`/post/${post.uid}`} key={post.uid}>
+                <a className={styles.post}>
+                  <strong>{post.data.title}</strong>
+                  <p>{post.data.subtitle}</p>
+                  <ul>
+                    <li>
+                      <FiCalendar />
+                      {post.first_publication_date}
+                    </li>
+                    <li>
+                      <FiUser />
+                      {post.data.author}
+                    </li>
+                  </ul>
+                </a>
+              </Link>
+            );
+          })}
+
+          { nextPage && (
+             <button type="button" onClick={handleNextPage}>
+             Carregar mais posts
+           </button>
+          )} 
+          
+          {/* como o next_page é uma string, ou ele vem com uma string da proxima página ou nulo.
+                os && fazem o papael do "if", se o next page tem conteúdo passa no IF, se vier sem conteúdo, ou seja. null
+                o button nao é exibido  */ }
+
+        </div>
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      pageSize: 1,
+    }
+  );
+
+  const posts = response.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: post.first_publication_date,
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
+
+  const postsPagination = {
+    next_page: response.next_page,
+    results: posts,
+  };
+
+  return {
+    props: { postsPagination },
+    revalidate: 60 * 60 // a cada 1 hora é atualizado a página
+  };
+};
